80613ad41772f1f02936171baf192f7083459aff
birthelambrechts <50906341+birthelambrechts@users.noreply.github.com>
2019-08-22 12:16:31 +0200

    Add files via upload

---
+++ game.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blackjack - Game</title>
+</head>
+<body>
+
+<form method="get">
+        <label for="submit">What would you like to do?</label> <br>
+        <input type="submit" name="hitButton" value="Hit me!">
+        <input type="submit" name="standButton" value="Stand">
+        <input type="submit" name="foldButton" value="Fold">
+    </form>
+
+<?php  
+
+?>
+
+</body>
+</html>
